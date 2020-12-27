4c37560c06b56450f527a0fbadcd98bf624ba530
Jang JunHee <lyradortiz913@naver.com>
2020-12-27 14:17:30 +0900

    #4.0~#4.4

---
+++ ver2/main.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+
+app = Flask("SupperScrapper")
+
+# @app.route("/")         #데코레이터(@): 바로 아래에 있는 "함수" 찾음.
+# def home():
+#     return "Hello! Welcome to mi casa!"
+
+# @app.route("/<username>")      #<>:placeholder 함수에서 사용되어야함.       //dynamic url
+# def contact(username):
+#     return f"Hello {username} how are you doing"
+@app.route("/")
+def home():
+        return render_template("home.html")
+
+@app.route("/report")
+def report():
+    word = request.args.get('word')
+    return render_template("report.html",searchingBy=word)
+    
+app.run(host = "localhost")
